2c50ae9279248360e99c4403d85ef5320b8201fb
Ayas Ibrahim <aayasabraham@gmail.com>
2026-05-11 19:48:49 +0700

    Building serverwith node js and Route handling with HTTP request and response

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import {
+  createServer,
+  IncomingMessage,
+  Server,
+  ServerResponse,
+} from "node:http";
+
+const server: Server = createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    // console.log(req?.url);// "/","/user","/products",
+    // console.log(req?.method)// "GET","POST","PATCH","DELETE"
+    const url = req.url;
+    const method = req.method;
+    if (url === "/" && method === "GET") {
+      res.writeHead(200, { "content-type": "Application/json" });
+      res.end(JSON.stringify({ message: "This is Hoome Route" }));
+    } else if (url?.startsWith("/products")) {
+      res.writeHead(200, {
+        "content-type": "text/plain",
+      });
+      res.end("This is Product Page");
+    } else {
+      res.writeHead(404, {
+        "content-type": "Application/json",
+      });
+      res.end(JSON.stringify({ message: "Page Not Found" }));
+    }
+  },
+);
+const port = 3000;
+server.listen(port, () => {
+  console.log(`server is running is ${port} `);
+});
