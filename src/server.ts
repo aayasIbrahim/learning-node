650d6fc8141597dfcd973a1f1f5aeff98f30177a
Ayas Ibrahim <aayasabraham@gmail.com>
2026-05-11 21:35:32 +0700

    Fix console log formatting in server startup message

--- src/server.ts
+++ src/server.ts
@@ -29,5 +29,5 @@ const server: Server = createServer(
 );
 const port = 3000;
 server.listen(port, () => {
-  console.log(`server is running is ${port} `);
+  console.log(`server is running is ${port}`);
 });
